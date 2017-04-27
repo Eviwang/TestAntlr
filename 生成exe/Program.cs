22ae8f1660a6d30626ad6c654de489e52cfec0ea
Evi.wang <Evi.wang@CY-EVIWANG>
2017-04-27 21:09:52 +0800

    update

---
+++ 生成exe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 生成exe
+{
+    class Program
+    {
+        //bin 目录下生成了一个Program.exe
+        static void Main(string[] args)
+        {
+            var assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(new AssemblyName("Assembly"), AssemblyBuilderAccess.RunAndSave);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("Program", "Program.exe", true);
+
+            var typeBuilder = moduleBuilder.DefineType("Foo", TypeAttributes.Public | TypeAttributes.Class);
+            
+            var method = typeBuilder.DefineMethod("Main", MethodAttributes.Public | MethodAttributes.Static, typeof(void), Type.EmptyTypes);
+
+            ILGenerator methodIL = method.GetILGenerator();
+            methodIL.Emit(OpCodes.Ldstr,"hello world!");
+            methodIL.Emit(OpCodes.Call,typeof(Console).GetMethod(nameof(Console.WriteLine), new[] { typeof(string) }));
+            //methodIL.Emit(OpCodes.Call, typeof(Console).GetMethod(nameof(Console.ReadLine)));
+            methodIL.Emit(OpCodes.Ret);
+            assemblyBuilder.SetEntryPoint(method, PEFileKinds.ConsoleApplication);
+            typeBuilder.CreateType();
+            assemblyBuilder.Save("Program.exe");
+        }
+    }
+}
